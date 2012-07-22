5b15095a689b635062f7bb673640d8c674928638
Nepomuk Fraedrich <info@nepda.eu>
2012-07-22 18:42:25 +0200

    - Komplett kommentiert.

--- srv.py
+++ srv.py
@@ -1,60 +1,77 @@
 import sys
 
-cfg_index_name = "index_html"
+cfg_index_name = "index_html" # legt die Standardresource fest (Datei: resource_index_html.py)
 
 def main(conn, data):
 	print "\r\n\r\n- - - - request for nepdaPy server - - - - - - - - - - - - - - "
 	
-	request = parseHeader(data)
-	#print request['resource']
+	request = parseHeader(data) # sucht relevante infos aus dem Header
+	#
+	# Beispiel reuqest: http://localhost:5001/index.html?param1=value1&param2=val2
+	# wuerde folgendes ergeben:
+	# reuqest = {'resource': '/index.html', 'version': '1.1', 'protocol': 'HTTP', 'method': 'GET', 'params': {'param1': 'value1', 'param2': 'val2'}}
 	
-	resource = request['resource'][1:]
+	resource = request['resource'][1:] # Resource ohne den anfangs Slash
 	
+	# Wenn also keine Resource explizit angegeben wurde
 	if request['resource'] == "/" :
-		resource = cfg_index_name
+		resource = cfg_index_name # standard Seite laden
 	
 	try:
-		mod = __import__("resource_" + resource.replace(".", "_"))
-		reload(mod)
+		mod = __import__("resource_" + resource.replace(".", "_")) # Importiert die Datei "resource_<module_name>"
+		reload(mod) # laedt die Datei neu, falls sie geaendert wurde (koennte man rausnehmen, wenn der Server komplett fertig geschrieben ist
 		try:
-			mod.main(conn, request)
+			mod.main(conn, request)                      # ACHTUNG! Jede Resource MUSS die Funktion main(self, connection, request) anbieten. Dies ist der Einstiegspunkt
 		except:
-			print "Unexpected error:", sys.exc_info()
-	except ImportError:
+			print "Unexpected module error", sys.exc_info()
+	except ImportError: # Wenn das Modul nicht gefunden wurde
 		print "The resource %s was not found"%request['resource']
-		conn.send("HTTP/1.0 404 Not Found\r\n")
-		conn.send("Content-Type: text/html; charset=utf8\r\n")
-		conn.send("\r\n")
-		conn.send("<h1>Not Found</h1><p>The requested URL %s was not found on this server</p>"%request['resource'])
+		status404(conn, request)
 	except:
-		print "404 not found:", sys.exc_info()[0]
-		conn.send("HTTP/1.0 500 OK\r\n")
-		conn.send("Content-Type: text/html; charset=utf8\r\n")
-		conn.send("\r\n")
-		conn.send("<h1>404 Resource not found</h1>")
+		print "500 not found:", sys.exc_info()[0]
+		status500(conn, request)
 	print "\r\n\r\n- - - - request end : nepdaPy server - - - - - - - - - - - - - "
 
 def parseHeader(data):
 	#print "srv.parseHeader hat folgende Daten:\n"
 	#print data
 	
-	header_and_body = data.split("\r\n\r\n")
+	header_and_body = data.split("\r\n\r\n") # header und body trennen
 	
 	http_header = header_and_body[0]
-	#http_body = header_and_body[1]
+	#http_body = header_and_body[1] # body brauchen wir hier nicht
 	
-	header_lines = http_header.split("\r\n")
+	print http_header
 	
-	h_first_line = header_lines[0]
+	header_lines = http_header.split("\r\n") # Header in einzelne Zeilen aufteilen
+	
+	h_first_line = header_lines[0] # erste Zeile, die hier fuer uns extrem wichtig ist
 	
 	space_parts = h_first_line.split(" ")
 	
+	
+	# Jede HTTP-Anfrage sieht so aehnlich aus:
+	#
+	# GET /resource_name.html HTTP/1.0
+	# ^  ^ ^                 ^ ^ 
+	# |  | |                 | |
+	# |  | |                 | + Protokoll/Version
+	# |  | |                 + Leerzeichen
+	# |  | + Resource
+	# |  + Leerzeichen
+	# + Method
+	#
+	# Ergibt also exakt 3 Parts, die durch ein Leerzeichen getrennt sind
 	if len(space_parts) != 3 :
 		print "kein gueltige http anfrage"
 		return
 	
 	http_method = space_parts[0]
 	http_request_file = space_parts[1]
+	
+	http_protocol = space_parts[2].split("/")[0] # Protokoll und P.-Version sind immer durch ein / getrennt
+	http_version = space_parts[2].split("/")[1]
+	
 	url_params = {} # dict
 	
 	# Wenn (ein) URL Parameter angegeben sind/ist ist dies eindeutig an dem Fragezeichen zu erkennen.
@@ -72,11 +89,11 @@ def parseHeader(data):
 		else:
 			params = [params]
 			# params koennte so aussehen: params = ['a=val']
+		
+		for key_value_pair in params: # params = ['a=val', 'b=other']
 			
-		print params
-			
-		for key_value_pair in params:
-			if key_value_pair.find("=") > 0:
+			# z.B.: key_value_pair = 'a=val'
+			if key_value_pair.find("=") > 0:  # Parameter koennen auch OHNE Wert angegeben werden...!
 				key = key_value_pair.split("=")[0]
 				value = key_value_pair.split("=")[1]
 			else:
@@ -85,9 +102,7 @@ def parseHeader(data):
 				
 			url_params[key] = value
 	
-	http_protocol = space_parts[2].split("/")[0]
-	http_version = space_parts[2].split("/")[1]
-	
+	# Zur Kontrolle / Uebersicht einfach mal alles ausgeben
 	print "URL-Parameter: %s"%url_params
 	print "HTTP-Protcol: %s"%http_protocol
 	print "HTTP-Version: %s"%http_version
@@ -96,6 +111,20 @@ def parseHeader(data):
 	
 	request = {"protocol":http_protocol, "version":http_version, "method":http_method, "resource":http_request_file, "params":url_params}
 	return request
+
+# ausgelagerte 404 Status Funktion
+def status404(conn, request):
+	conn.send("HTTP/1.0 404 Not Found\r\n")
+	conn.send("Content-Type: text/html; charset=utf8\r\n")
+	conn.send("\r\n")
+	conn.send("<h1>Not Found</h1><p>The requested URL %s was not found on this server</p>"%request['resource'])
+
+# ausgelagerte 500 Status Funktion
+def status500(conn, request):
+	conn.send("HTTP/1.0 500 Server Error\r\n")
+	conn.send("Content-Type: text/html; charset=utf8\r\n")
+	conn.send("\r\n")
+	conn.send("<h1>Server Error</h1><p>The requested URL %s created a fatal error 500 on this server</p>"%request['resource'])
 
 def status():
 	return "ok"
